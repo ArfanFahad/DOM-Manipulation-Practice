336d0acce89b81e861527e2502be6245da8ccfb2
Md Fahad Farazi <arfanfahad40@gmail.com>
2024-08-26 22:06:09 +0600

    finalize the project but couldn't finish something which is 'to remove the existing input from input folder

--- Create Element Append Element/script.js
+++ Create Element Append Element/script.js
@@ -1,13 +1,24 @@
 
 
 
+
+
+
 function addItems () {
     // First picking the input value
-
-
+    const inputValue = document.getElementById("listInput").value; 
+    
+    
     //Creating the list items 
-
+     const createList = document.createElement("li"); 
+     createList.innerHTML = inputValue; 
+          
 
     // Then insert the value in the ul tag 
-
+    const insertList = document.getElementById("listItems"); 
+    insertList.appendChild(createList); 
 }
+
+
+
+
