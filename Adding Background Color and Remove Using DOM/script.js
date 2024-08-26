528c656bd3da476e1e1fde1b9a6f5bbe159fa8b5
Md Fahad Farazi <arfanfahad40@gmail.com>
2024-08-26 21:39:00 +0600

    in Script.js, I have optimized some code

--- Adding Background Color and Remove Using DOM/script.js
+++ Adding Background Color and Remove Using DOM/script.js
@@ -3,19 +3,16 @@
 
 
 
-
+const heading = document.getElementById("header"); 
 
 
 function add_style() {
-    const heading = document.getElementById("header"); 
     heading.classList.add('btnOneStyle'); 
 }
 
 function remove_style() {
-    const heading = document.getElementById("header"); 
     heading.classList.remove('btnOneStyle'); 
 }
-
 
 
 
