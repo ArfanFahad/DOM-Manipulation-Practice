3053038bc90ed8729a524b9073560e59d3cff455
Md Fahad Farazi <arfanfahad40@gmail.com>
2024-08-26 10:10:29 +0600

    Make a project that will add background color by clicking a button and remove that color using another button

---
+++ Adding Background Color and Remove Using DOM/script.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+function add_style() {
+    const heading = document.getElementById("header"); 
+    heading.classList.add('btnOneStyle'); 
+}
+
+function remove_style() {
+    const heading = document.getElementById("header"); 
+    heading.classList.remove('btnOneStyle'); 
+}
+
+
+
+
